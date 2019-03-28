d6f43c6c683a8eaa4da86a8e0e3201f760f08dd3
YouTwitFace <YouTwitFaceTG@gmail.com>
2019-03-27 20:38:21 -0400

    Add npm package

---
+++ node-library/index.js
@@ -0,0 +1,36 @@
+const { EventEmitter } = require(`events`);
+const io = require(`socket.io-client`);
+
+const SOCKET_API_URL = `https://lungers-chat-api.now.sh`;
+
+class LungersChat extends EventEmitter {
+    constructor() {
+        super();
+        this.socket = null;
+    }
+
+    connect(name) {
+        if (this.socket !== null) throw new Error(`Already connected to chat.`);
+
+        const socket = this.socket = io.connect(SOCKET_API_URL);
+        socket.emit(`set-name`, name);
+
+        socket.on(`connect`, () => this.emit(`connect`));
+        socket.on(`user-connected`, users => this.emit(`user-connected`, users));
+        socket.on(`user-disconnected`, id => this.emit(`user-disconnected`, id));
+
+        socket.on(`message`, message => {
+            this.emit(`message`, {
+                date: message.date,
+                user: message.user,
+                text: message.message,
+            });
+        });
+    }
+
+    sendMessage(text) {
+        this.socket.emit(`message`, text);
+    }
+}
+
+module.exports = LungersChat;
